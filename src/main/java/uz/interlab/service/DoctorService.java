29970b78f01fd204fd80e2584d3c25df6d32db92
SuperHuma <theferganes@gmail.com>
2024-07-03 16:37:37 +0500

    commit-7

--- src/main/java/uz/interlab/service/DoctorService.java
+++ src/main/java/uz/interlab/service/DoctorService.java
@@ -7,10 +7,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
-import uz.interlab.entity.Doctor;
 import uz.interlab.entity.Photo;
+import uz.interlab.entity.doctor.Doctor;
 import uz.interlab.payload.ApiResponse;
-import uz.interlab.payload.DoctorDTO;
+import uz.interlab.payload.doctor.DoctorDTO;
 import uz.interlab.respository.DoctorRepository;
 
 import java.util.ArrayList;
