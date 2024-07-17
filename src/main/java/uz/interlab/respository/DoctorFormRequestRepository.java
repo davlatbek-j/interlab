b811b47c1f0dcd6c9b7a4bee71d72ed9297e17a6
SuperHuma <theferganes@gmail.com>
2024-07-17 18:16:46 +0500

    Form Request (Answers)

--- src/main/java/uz/interlab/respository/DoctorFormRequestRepository.java
+++
@@ -1,8 +0,0 @@
-package uz.interlab.respository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import uz.interlab.entity.form.doctorPage.DoctorFormRequest;
-
-public interface DoctorFormRequestRepository extends JpaRepository<DoctorFormRequest, Long>
-{
-}
