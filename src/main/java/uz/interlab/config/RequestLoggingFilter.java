473968908889a61d857ddcf59d8408ad38571903
SuperHuma <theferganes@gmail.com>
2024-08-08 14:43:04 +0500

    request logging

---
+++ src/main/java/uz/interlab/config/RequestLoggingFilter.java
@@ -0,0 +1,30 @@
+package uz.interlab.config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class RequestLoggingFilter implements Filter
+{
+
+    private static final Logger logger = LoggerFactory.getLogger(RequestLoggingFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException
+    {
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        logger.info("Incoming request: method={}, uri={}",
+                httpRequest.getMethod(),
+                httpRequest.getRequestURI());
+
+        chain.doFilter(request, response);
+    }
+
+}
